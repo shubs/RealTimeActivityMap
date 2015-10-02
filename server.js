9beac794ce2e7da0a2cdcf774acec0d8de219d8f
SHARMA Shubham <shubham@sharma.fr>
2015-10-02 13:39:36 +0200

    server ok

--- server.js
+++ server.js
@@ -4,12 +4,57 @@ var socketio  = require('socket.io');
 var Chance = require('chance');
 var moment = require('moment');
 
+var satelize = require('satelize');
+var bodyParser = require('body-parser'); 
+
 var chance = new Chance();
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	  extended: true
+}));
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
+});
+
+app.post('/signup', function(req, res) {
+	var ip = req.body.context.ip;
+	var type = req.body.type;
+	var event = req.body.event;
+
+	if (type == 'track'){
+		console.log("ip \t-> " + ip);
+		console.log("type \t-> " + type);
+		console.log("event \t-> " + event);
+
+		satelize.satelize({ip:ip}, function(err, geoData) {
+			if (err) { console.log(err); }
+			else {
+				var obj = JSON.parse(geoData);
+
+				var country = obj.country;
+				var longitude = obj.longitude;
+				var latitude = obj.latitude;
+
+				coordinates = {
+					event : event,
+					latitude : latitude,
+					longitude : longitude,
+					country: country,
+					ip: ip,
+					type: plans[chance.natural({min: 0, max: (plans.length - 1)})],
+					time: moment().format('hh:mm:ss')
+				};
+
+				io.emit('new', coordinates);
+
+				console.log(obj);
+				console.log("\n");
+			}
+		});
+	}
 });
 
 var server    = app.listen(3000);
@@ -20,16 +65,16 @@ io.sockets.on('connection', function (socket) {
   console.log('a user connected');
 });
 
-setInterval(function(){
-	coordinates = {
-		latitude : chance.latitude(),
-		longitude : chance.longitude(),
-		country: chance.country({ full: true }),
-		ip: chance.ip(),
-		type: plans[chance.natural({min: 0, max: (plans.length - 1)})],
-		time: moment().format('hh:mm:ss')
-	};
+// setInterval(function(){
+// 	coordinates = {
+// 		latitude : chance.latitude(),
+// 		longitude : chance.longitude(),
+// 		country: chance.country({ full: true }),
+// 		ip: chance.ip(),
+// 		type: plans[chance.natural({min: 0, max: (plans.length - 1)})],
+// 		time: moment().format('hh:mm:ss')
+// 	};
 
-	io.emit('new', coordinates);
-}, 1000);
+// 	io.emit('new', coordinates);
+// }, 1000);
 //}, chance.natural({min: 4000, max: 20000}));
