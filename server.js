70f6f7490f28b7d4e386410c05818a14acdf0835
SHARMA Shubham <shubham@sharma.fr>
2015-10-06 14:13:30 +0200

    bug app.use

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var chance = new Chance();
 var number = 0;
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 	  extended: true
 }));
 
